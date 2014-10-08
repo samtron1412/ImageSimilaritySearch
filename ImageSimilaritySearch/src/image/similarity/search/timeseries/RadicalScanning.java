20b2d06abc3ba486e29b4e871e7ffcceb1054e66
Thaison Tranhuynh <samtron1412@gmail.com>
2014-10-08 22:43:21 +0700

    Try new branch image_to_timeseries

--- ImageSimilaritySearch/src/image/similarity/search/timeseries/RadicalScanning.java
+++ ImageSimilaritySearch/src/image/similarity/search/timeseries/RadicalScanning.java
@@ -2,4 +2,7 @@ package image.similarity.search.timeseries;
 
 public class RadicalScanning {
 //TODO: coding function to convert image contour to time series
+	public static void main(String[] args) {
+		System.out.print("Hello");
+	}
 }
