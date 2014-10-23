2e21e1ea35dc0aa0cb57ec52eda9e351a01c7eb6
Thaison Tranhuynh <samtron1412@gmail.com>
2014-10-22 23:33:54 -0700

    Finish design with lazy code generate of WindowBuilder.

--- ImageSimilaritySearch/src/image/similarity/search/gui/swing/DemoSwingGui.java
+++ ImageSimilaritySearch/src/image/similarity/search/gui/swing/DemoSwingGui.java
@@ -24,11 +24,21 @@ import java.awt.event.MouseEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
+import java.awt.event.MouseListener;
 
 @SuppressWarnings("serial")
 public class DemoSwingGui extends JFrame {
 
   private JPanel contentPane;
+  private JButton btnChooseFirstImage;
+  private JButton btnChooseSecondImage;
+  private JPanel pnShowFirstImage;
+  private JPanel pnShowSecondImage;
+  private JLabel lblCompareResult;
+  private JLabel lblDistanceBetweenImages;
+  private JButton btnCompare;
+  private JLabel lblShowResult;
+  private JLabel lblShowDistance;
 
   /**
    * Launch the application.
@@ -46,7 +56,7 @@ public class DemoSwingGui extends JFrame {
               break;
             }
           }
-          
+
           /**
            * INIT FRAME AND SET IT VISIBLE
            */
@@ -63,139 +73,140 @@ public class DemoSwingGui extends JFrame {
    * Create the frame.
    */
   public DemoSwingGui() {
-    /**
-     * INIT FRAME AND CONTENT PANE
-     */
+    initGUI();
+  }
+
+  private void initGUI() {
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setBounds(100, 100, 700, 500);
     contentPane = new JPanel();
     contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
     setContentPane(contentPane);
     GridBagLayout gbl_contentPane = new GridBagLayout();
-    gbl_contentPane.columnWidths = new int[] { 30, 134, 115, 30, 289, 30, 0 };
-    gbl_contentPane.rowHeights = new int[] { 27, 25, 28, 237, 28, 22, 32, 28, 0 };
-    gbl_contentPane.columnWeights = new double[] { 0.0, 1.0, 0.0, 0.0, 1.0,
+    gbl_contentPane.columnWidths = new int[] { 30, 160, 112, 30, 272, 30, 0 };
+    gbl_contentPane.rowHeights = new int[] { 30, 0, 30, 0, 30, 30, 30, 30, 0 };
+    gbl_contentPane.columnWeights = new double[] { 0.0, 0.0, 1.0, 0.0, 1.0,
         0.0, Double.MIN_VALUE };
     gbl_contentPane.rowWeights = new double[] { 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
         0.0, 0.0, Double.MIN_VALUE };
     contentPane.setLayout(gbl_contentPane);
-
-    /**
-     * BUTTON BROWSE IMAGE 1
-     */
-    JButton btnChooseImage1 = new JButton("Choose Image 1");
-    btnChooseImage1.addMouseListener(new MouseAdapter() {
-      @Override
-      public void mouseClicked(MouseEvent btnChooseImage1Event) {
-        
-      }
-    });
-    GridBagConstraints gbc_btnChooseImage1 = new GridBagConstraints();
-    gbc_btnChooseImage1.gridwidth = 2;
-    gbc_btnChooseImage1.insets = new Insets(0, 0, 5, 5);
-    gbc_btnChooseImage1.gridx = 1;
-    gbc_btnChooseImage1.gridy = 1;
-    contentPane.add(btnChooseImage1, gbc_btnChooseImage1);
-
-    /**
-     * BUTTON BROWSE IMAGE 2
-     */
-    JButton btnChooseImage2 = new JButton("Choose Image 2");
-    GridBagConstraints gbc_btnChooseImage2 = new GridBagConstraints();
-    gbc_btnChooseImage2.insets = new Insets(0, 0, 5, 5);
-    gbc_btnChooseImage2.gridx = 4;
-    gbc_btnChooseImage2.gridy = 1;
-    contentPane.add(btnChooseImage2, gbc_btnChooseImage2);
-
-    /**
-     * PANEL 1 TO SHOW IMAGE 1
-     */
-    JPanel panel1 = new JPanel();
-    panel1.setBackground(Color.WHITE);
-    GridBagConstraints gbc_panel1 = new GridBagConstraints();
-    gbc_panel1.gridwidth = 2;
-    gbc_panel1.insets = new Insets(0, 0, 5, 5);
-    gbc_panel1.fill = GridBagConstraints.BOTH;
-    gbc_panel1.gridx = 1;
-    gbc_panel1.gridy = 3;
-    contentPane.add(panel1, gbc_panel1);
-    
-    JLabel lblShowImage1 = new JLabel("");
-    lblShowImage1.setHorizontalAlignment(SwingConstants.CENTER);
-    lblShowImage1.setVerticalAlignment(SwingConstants.CENTER);
-    panel1.add(lblShowImage1);
-
-    /**
-     * PANE 2 TO SHOW IMAGE 2
-     */
-    JPanel panel2 = new JPanel();
-    panel2.setBackground(Color.WHITE);
-    GridBagConstraints gbc_panel2 = new GridBagConstraints();
-    gbc_panel2.insets = new Insets(0, 0, 5, 5);
-    gbc_panel2.fill = GridBagConstraints.BOTH;
-    gbc_panel2.gridx = 4;
-    gbc_panel2.gridy = 3;
-    contentPane.add(panel2, gbc_panel2);
-    
-    JLabel lblShowImage2 = new JLabel("");
-    lblShowImage2.setHorizontalAlignment(SwingConstants.CENTER);
-    lblShowImage2.setVerticalAlignment(SwingConstants.CENTER);
-    panel2.add(lblShowImage2);
-
-    /**
-     * LABEL RESULT
-     */
-    JLabel lblResult = new JLabel("Compare Result: ");
-    GridBagConstraints gbc_lblResult = new GridBagConstraints();
-    gbc_lblResult.anchor = GridBagConstraints.EAST;
-    gbc_lblResult.insets = new Insets(0, 0, 5, 5);
-    gbc_lblResult.gridx = 1;
-    gbc_lblResult.gridy = 5;
-    contentPane.add(lblResult, gbc_lblResult);
-
-    /**
-     * LABEL TO SHOW RESULT AFTER COMPARE
-     */
-    JLabel lblShowResult = new JLabel("0 %");
-    lblShowResult.setBackground(Color.GRAY);
-    GridBagConstraints gbc_lblShowResult = new GridBagConstraints();
-    gbc_lblShowResult.anchor = GridBagConstraints.WEST;
-    gbc_lblShowResult.insets = new Insets(0, 0, 5, 5);
-    gbc_lblShowResult.gridx = 2;
-    gbc_lblShowResult.gridy = 5;
-    contentPane.add(lblShowResult, gbc_lblShowResult);
-
-    /**
-     * BUTTON TO COMPARE TWO IMAGE
-     */
-    JButton btnCompare = new JButton("Compare");
-    GridBagConstraints gbc_btnCompare = new GridBagConstraints();
-    gbc_btnCompare.insets = new Insets(0, 0, 5, 5);
-    gbc_btnCompare.gridx = 4;
-    gbc_btnCompare.gridy = 5;
-    contentPane.add(btnCompare, gbc_btnCompare);
-
-    /**
-     * LABEL DISTANCE
-     */
-    JLabel lblDistance = new JLabel("Distance Between Images: ");
-    GridBagConstraints gbc_lblDistance = new GridBagConstraints();
-    gbc_lblDistance.anchor = GridBagConstraints.EAST;
-    gbc_lblDistance.insets = new Insets(0, 0, 5, 5);
-    gbc_lblDistance.gridx = 1;
-    gbc_lblDistance.gridy = 6;
-    contentPane.add(lblDistance, gbc_lblDistance);
-
-    /**
-     * LABEL TO SHOW DISTANCE AFTER COMPARE
-     */
-    JLabel lblShowDistance = new JLabel("0");
-    GridBagConstraints gbc_lblShowDistance = new GridBagConstraints();
-    gbc_lblShowDistance.anchor = GridBagConstraints.WEST;
-    gbc_lblShowDistance.insets = new Insets(0, 0, 5, 5);
-    gbc_lblShowDistance.gridx = 2;
-    gbc_lblShowDistance.gridy = 6;
-    contentPane.add(lblShowDistance, gbc_lblShowDistance);
+    GridBagConstraints gbcBtnChooseFirstImage = new GridBagConstraints();
+    gbcBtnChooseFirstImage.gridwidth = 2;
+    gbcBtnChooseFirstImage.insets = new Insets(0, 0, 5, 5);
+    gbcBtnChooseFirstImage.gridx = 1;
+    gbcBtnChooseFirstImage.gridy = 1;
+    contentPane.add(getBtnChooseFirstImage(), gbcBtnChooseFirstImage);
+    GridBagConstraints gbcBtnChooseSecondImage = new GridBagConstraints();
+    gbcBtnChooseSecondImage.insets = new Insets(0, 0, 5, 5);
+    gbcBtnChooseSecondImage.gridx = 4;
+    gbcBtnChooseSecondImage.gridy = 1;
+    contentPane.add(getBtnChooseSecondImage(), gbcBtnChooseSecondImage);
+    GridBagConstraints gbcPnShowFirstImage = new GridBagConstraints();
+    gbcPnShowFirstImage.gridwidth = 2;
+    gbcPnShowFirstImage.insets = new Insets(0, 0, 5, 5);
+    gbcPnShowFirstImage.fill = GridBagConstraints.BOTH;
+    gbcPnShowFirstImage.gridx = 1;
+    gbcPnShowFirstImage.gridy = 3;
+    contentPane.add(getPnShowFirstImage(), gbcPnShowFirstImage);
+    GridBagConstraints gbcPnShowSecondImage = new GridBagConstraints();
+    gbcPnShowSecondImage.insets = new Insets(0, 0, 5, 5);
+    gbcPnShowSecondImage.fill = GridBagConstraints.BOTH;
+    gbcPnShowSecondImage.gridx = 4;
+    gbcPnShowSecondImage.gridy = 3;
+    contentPane.add(getPnShowSecondImage(), gbcPnShowSecondImage);
+    GridBagConstraints gbcLblCompareResult = new GridBagConstraints();
+    gbcLblCompareResult.anchor = GridBagConstraints.EAST;
+    gbcLblCompareResult.insets = new Insets(0, 0, 5, 5);
+    gbcLblCompareResult.gridx = 1;
+    gbcLblCompareResult.gridy = 5;
+    contentPane.add(getLblCompareResult(), gbcLblCompareResult);
+    GridBagConstraints gbcLblShowResult = new GridBagConstraints();
+    gbcLblShowResult.anchor = GridBagConstraints.WEST;
+    gbcLblShowResult.insets = new Insets(0, 0, 5, 5);
+    gbcLblShowResult.gridx = 2;
+    gbcLblShowResult.gridy = 5;
+    contentPane.add(getLblShowResult(), gbcLblShowResult);
+    GridBagConstraints gbcBtnCompare = new GridBagConstraints();
+    gbcBtnCompare.insets = new Insets(0, 0, 5, 5);
+    gbcBtnCompare.gridx = 4;
+    gbcBtnCompare.gridy = 5;
+    contentPane.add(getBtnCompare(), gbcBtnCompare);
+    GridBagConstraints gbcLblDistanceBetweenImages = new GridBagConstraints();
+    gbcLblDistanceBetweenImages.anchor = GridBagConstraints.EAST;
+    gbcLblDistanceBetweenImages.insets = new Insets(0, 0, 5, 5);
+    gbcLblDistanceBetweenImages.gridx = 1;
+    gbcLblDistanceBetweenImages.gridy = 6;
+    contentPane.add(getLblDistanceBetweenImages(), gbcLblDistanceBetweenImages);
+    GridBagConstraints gbcLblShowDistance = new GridBagConstraints();
+    gbcLblShowDistance.anchor = GridBagConstraints.WEST;
+    gbcLblShowDistance.insets = new Insets(0, 0, 5, 5);
+    gbcLblShowDistance.gridx = 2;
+    gbcLblShowDistance.gridy = 6;
+    contentPane.add(getLblShowDistance(), gbcLblShowDistance);
   }
 
+  public JButton getBtnChooseFirstImage() {
+    if (btnChooseFirstImage == null) {
+      btnChooseFirstImage = new JButton("Choose First Image");
+    }
+    return btnChooseFirstImage;
+  }
+
+  public JButton getBtnChooseSecondImage() {
+    if (btnChooseSecondImage == null) {
+      btnChooseSecondImage = new JButton("Choose Second Image");
+    }
+    return btnChooseSecondImage;
+  }
+
+  public JPanel getPnShowFirstImage() {
+    if (pnShowFirstImage == null) {
+      pnShowFirstImage = new JPanel();
+      pnShowFirstImage.setBackground(Color.WHITE);
+    }
+    return pnShowFirstImage;
+  }
+
+  public JPanel getPnShowSecondImage() {
+    if (pnShowSecondImage == null) {
+      pnShowSecondImage = new JPanel();
+      pnShowSecondImage.setBackground(Color.WHITE);
+    }
+    return pnShowSecondImage;
+  }
+
+  public JLabel getLblCompareResult() {
+    if (lblCompareResult == null) {
+      lblCompareResult = new JLabel("Compare Result: ");
+    }
+    return lblCompareResult;
+  }
+
+  public JLabel getLblDistanceBetweenImages() {
+    if (lblDistanceBetweenImages == null) {
+      lblDistanceBetweenImages = new JLabel("Distance Between Images: ");
+    }
+    return lblDistanceBetweenImages;
+  }
+
+  public JButton getBtnCompare() {
+    if (btnCompare == null) {
+      btnCompare = new JButton("Compare");
+    }
+    return btnCompare;
+  }
+
+  public JLabel getLblShowResult() {
+    if (lblShowResult == null) {
+      lblShowResult = new JLabel("0 %");
+    }
+    return lblShowResult;
+  }
+
+  public JLabel getLblShowDistance() {
+    if (lblShowDistance == null) {
+      lblShowDistance = new JLabel("0");
+    }
+    return lblShowDistance;
+  }
 }
